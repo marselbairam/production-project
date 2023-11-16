d7a8eb414f17ac906ef19329cad2bc68c2becc01
Marsel <marselbairam@gmail.com>
2023-11-16 13:36:54 +0300

    add ui components

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,9 +1,9 @@
 import React from 'react';
-import {Link} from 'react-router-dom';
 
 import {useTheme} from "app/providers/ThemeProvider";
 import {classNames} from "shared/lib/classNames/classNames";
 
+import {Navbar} from "widgets/Navbar";
 import {AppRouter} from "app/providers/router";
 
 import './styles/index.scss';
@@ -13,10 +13,9 @@ const App = () => {
 
   return (
     <div className={classNames('app', {}, [theme])}>
-      <button onClick={toggleTheme}>TOGGLE</button>
-      <Link to="/">Главная</Link>
-      <Link to="/about">О сайте</Link>
+      <Navbar />
       <AppRouter />
+      <button onClick={toggleTheme}>TOGGLE</button>
     </div>
   );
 };
