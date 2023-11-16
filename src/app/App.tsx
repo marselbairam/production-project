15b7a68b1462ef8b2599a39983d652932f551d29
Marsel <marselbairam@gmail.com>
2023-11-16 09:35:38 +0300

    add FSD and absolute imports

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,12 +1,13 @@
 import React, {Suspense} from 'react';
 import {Link, Route, Routes} from 'react-router-dom';
 
-import {AboutPageAsync} from "./pages/AboutPage/AboutPage.async";
-import {MainPageAsync} from "./pages/MainPage/MainPage.async";
+import {MainPage} from "pages/MainPage";
+import {AboutPage} from "pages/AboutPage";
+
+import {useTheme} from "app/providers/ThemeProvider";
+import {classNames} from "shared/lib/classNames/classNames";
 
 import './styles/index.scss';
-import {useTheme} from "./theme/useTheme";
-import {classNames} from "./helpers/classNames/classNames";
 
 const App = () => {
   const {theme, toggleTheme} = useTheme()
@@ -18,8 +19,8 @@ const App = () => {
       <Link to="/about">О сайте</Link>
       <Suspense fallback={<div>Loading...</div>}>
         <Routes>
-          <Route path="/" element={<MainPageAsync />}/>
-          <Route path="/about" element={<AboutPageAsync />} />
+          <Route path="/" element={<MainPage />}/>
+          <Route path="/about" element={<AboutPage />} />
         </Routes>
       </Suspense>
     </div>
