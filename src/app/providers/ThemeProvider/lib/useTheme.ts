97037eaa75ad34fdd28e2b5bfd2989b6863c0758
Marsel <marselbairam@gmail.com>
2024-01-07 10:04:52 +0300

    add 404 page and page loader

--- src/app/providers/ThemeProvider/lib/useTheme.ts
+++ src/app/providers/ThemeProvider/lib/useTheme.ts
@@ -2,16 +2,15 @@ import { LOCAL_STORAGE_THEME_KEY, Theme, ThemeContext } from './ThemeContext'
 import { useContext } from 'react'
 
 interface UseThemeResult {
-  toggleTheme: () => void
   theme: Theme
+  toggleTheme: () => void
 }
-
 export function useTheme (): UseThemeResult {
   const { theme, setTheme } = useContext(ThemeContext)
 
   const toggleTheme = () => {
     const newTheme = theme === Theme.DARK ? Theme.LIGHT : Theme.DARK
-    setTheme(newTheme)
+    if (setTheme) setTheme(newTheme)
     localStorage.setItem(LOCAL_STORAGE_THEME_KEY, newTheme)
   }
 
